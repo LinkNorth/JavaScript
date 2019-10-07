5caf79b7b7128d433741d756550d3a7237038016
br0r <viktor@silfverstrom.com>
2019-10-07 12:35:26 +0200

    update

--- exercises/errors/fourth/fourth.js
+++ exercises/errors/fourth/fourth.js
@@ -1,8 +1,5 @@
 // Expected result: 120
-debugger;
-let filter = removeNumbersOverX([10, 20, 30, 40, 50], 30);
-let double = doubleAll(filter);
-let result = sum(double);
+let result = sum(doubleAll(removeNumbersOverX([10, 20, 30, 40, 50], 30)));
 console.log(result);
 
 // Return sum of all numbers in arr
@@ -14,18 +11,17 @@ function sum(arr) {
 
 // Double all numbers in arr
 function doubleAll(arr) {
-  let result =  arr.map(function(x) {
-    return x * 2;
+  return arr.map(function(x) {
+    x * 2;
   });
-  return result;
 }
 
 // Remove all numbers in arr that is over x
 function removeNumbersOverX(arr, x) {
   let newArray = [];
-  for (let i = 0; i < arr.length; i += 1) {
+  for (let i = 0; i < arr; i += 1) {
     let value = arr[i];
-    if (value <= x) {
+    if (value > x) {
       newArray.push(value);
     }
   }
